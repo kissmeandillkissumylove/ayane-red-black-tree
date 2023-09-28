126aea8664eb60736aa4540fba93bb4120cceafa
kissmeandillkissumylove <kissmeandillkissumylove@gmail.com>
2023-09-28 18:01:02 +0700

    add: delete();refactor few elements

--- RBTree.py
+++ RBTree.py
@@ -44,14 +44,14 @@ class RBTree:
 		nil: Nil node initialization
 		_lenght: len(RBtree)
 		"""
-		self.__nil: Nil = Nil()
-		self._root: Union[Node, Nil] = self.__nil
+		self._nil: Nil = Nil()
+		self._root: Union[Node, Nil] = self._nil
 		self._lenght: int = 0
 
 	def search(self, data: Union[int, float, str]) -> Union[Node, None]:
 		"""find element in the tree"""
 		node = self._root
-		while node != self.__nil:
+		while node != self._nil:
 			if data == node.data:
 				return node # found node
 			elif data > node.data:
@@ -65,20 +65,20 @@ class RBTree:
 		"""insert new node in the tree:
 		data: any value that we can compare"""
 		self._lenght += 1 # increase length
-		new_node = Node(data=data, left=self.__nil, right=self.__nil) # create new node
-		if self._root == self.__nil: self._root = new_node # set a new root if there is nil
+		new_node = Node(data=data, left=self._nil, right=self._nil) # create new node
+		if self._root == self._nil: self._root = new_node # set a new root if there is nil
 		else: # root is existing
 			prev = self._root # the parent for the new node
 			while True:
 				if new_node.data > prev.data:
-					if prev.right != self.__nil:
+					if prev.right != self._nil:
 						prev = prev.right # next node
 					else:
 						prev.right = new_node # set new node
 						new_node.prev = prev # set parent for the new node
 						break # leave the loop
 				elif new_node.data < prev.data:
-					if prev.left != self.__nil:
+					if prev.left != self._nil:
 						prev = prev.left # next node
 					else:
 						prev.left = new_node # set new node
@@ -87,9 +87,9 @@ class RBTree:
 				else: # if such a node already exists in the tree
 					self._lenght -= 1 # decrease lenght
 					return # exit the insert()
-		self.__fix(new_node) # fix the tree after new node inserting
+		self._fix(new_node) # fix the tree after new node inserting
 
-	def __fix(self, node: Node) -> NoReturn:
+	def _fix(self, node: Node) -> NoReturn:
 		"""fix the tree after inserting"""
 		while node != self._root and node.prev.red:
 			# be sure to check which side the uncle is on and look at his color.
@@ -104,10 +104,10 @@ class RBTree:
 				else: # uncle is black
 					if node == node.prev.left: # check node's position from parent
 						node = node.prev
-						self.__rotate_right(node) # need right rotate if node in left side
+						self._rotate_right(node) # need right rotate if node in left side
 					node.prev.red = False # if node already in left side set black for parent
 					node.prev.prev.red = True # red for parent's parent
-					self.__rotate_left(node.prev.prev) # left rotate parent's parent
+					self._rotate_left(node.prev.prev) # left rotate parent's parent
 			else: # uncle on the right side
 				if node.prev.prev.right.red: # check uncle's color
 					node.prev.prev.right.red = False # set black for uncle
@@ -117,19 +117,19 @@ class RBTree:
 				else: # uncle is not communist
 					if node == node.prev.right: # сheck node's position
 						node = node.prev
-						self.__rotate_left(node) #rotate parent to left side
+						self._rotate_left(node) #rotate parent to left side
 					node.prev.red = False # set parent black
 					node.prev.prev.red = True # set parent's parent red
-					self.__rotate_right(node.prev.prev) # right rotate for parent's parent
+					self._rotate_right(node.prev.prev) # right rotate for parent's parent
 		self._root.red = False # paint the root black
 
-	def __rotate_right(self, nodeX: Node) -> NoReturn:
+	def _rotate_right(self, nodeX: Node):
 		"""rotate the tree to the right relative to the incoming node.
 		we pull the node to the right."""
 		nodeY = nodeX.left # save Y is X left child
 		nodeX.left = nodeY.right # set new child for X is Y right child
 
-		if nodeX.left != self.__nil: # if new X child is not Nil-set new parent for new X child
+		if nodeX.left != self._nil: # if new X child is not Nil-set new parent for new X child
 			nodeX.left.prev = nodeX
 
 		nodeY.prev = nodeX.prev # set X's parent for Y
@@ -143,14 +143,14 @@ class RBTree:
 		nodeY.right = nodeX # set Y' right child is X
 		nodeX.prev = nodeY # set new parent for X is Y
 
-	def __rotate_left(self, nodeX: Node) -> NoReturn:
+	def _rotate_left(self, nodeX: Node):
 		"""rotate the tree to the left relative to the incoming node.
 		we pull the node to the left."""
 		# check left_rotate() comments for info what is happening
 		nodeY = nodeX.right
 		nodeX.right = nodeY.left
 
-		if nodeX.right != self.__nil:
+		if nodeX.right != self._nil:
 			nodeX.right.prev = nodeX
 
 		nodeY.prev = nodeX.prev
@@ -164,76 +164,98 @@ class RBTree:
 		nodeY.left = nodeX
 		nodeX.prev = nodeY
 
-	def delete(self, data: Union[int, float, str]) -> None:
-		"""delete node from tree"""
-		self._lenght -= 1  # decrease lenght
+	def delete(self, data: Union[int, float, str]):
+		"""delete node from the tree. first try to get a node which has 0-2childs."""
 		node = self.search(data)
-		if node != None:  # if node is exist
-			if node.red:  # check is node red or black
-				if node.left == node.right: # if both childs are Nils
-					if node.prev.right == node:
-						node.prev.right = self.__nil
-						return
-					else:
-						node.prev.left = self.__nil
-						return
-				else:
-					nodeL, nodeR = node.left, node.right
-					while nodeL.right != self.__nil and nodeR.left != self.__nil:
-						if nodeL.right != self.__nil:
-							nodeL = nodeL.right
-						if nodeR.left != self.__nil:
-							nodeR = nodeR.left
-					if nodeL.red:
-						node.data = nodeL.data
-						nodeL.prev.right = self.__nil
-						return
-					elif nodeR.red:
-						node.data = nodeR.data
-						nodeR.prev.left = self.__nil
-						return
-					else: #both nodes are black
-						if nodeL.left != self.__nil:
-							node.data = nodeL.data
-							nodeL.data = nodeL.left.data
-							nodeL.left = self.__nil
-							return
-						elif nodeR.right != self.__nil:
-							node.data = nodeR.data
-							nodeR.data = nodeR.right.data
-							nodeR.right = self.__nil
-							return
-						else: # both black nodes have no childs
-							node.data = nodeL.data
-							if nodeL.prev.left != nodeL:
-								if nodeL.prev.left.red:
-									self.__rotate_right(nodeL.prev)
-									nodeL.prev.prev.red = False
-									nodeL.prev.left.red = True
-									nodeL.prev.right = self.__nil
-									self.__rotate_left(node)
-									return
-								else:
-									nodeL.prev.left.red = True
-									nodeL.prev.right = self.__nil
-									self.__rotate_left(node)
-									return
-							else:
-								nodeL.prev.red = False
-								nodeL.prev.right.red = True
-								nodeL.prev.left = self.__nil
-								return
-			else:  # node is black
-				if node.left == node.right:
-					raise ValueError
-				elif node.left == self.__nil:
-					raise ValueError
-				elif node.right == self.__nil:
-					raise ValueError
-				elif node.left != self.__nil and node.right != self.__nil:
-					raise ValueError
+		if node is None:
+			return
+		self._lenght -= 1
+		color = node.red
+		if node.left == self._nil:
+			nodetmp = node.right
+			self._swap_nodes(node, nodetmp)
+		elif node.right == self._nil:
+			nodetmp = node.left
+			self._swap_nodes(node, nodetmp)
 		else:
-			self._lenght += 1
+			successor = self._find_successor(node.right)
+			color = successor.red
+			nodetmp = successor.right
+			if successor.prev == node:
+				nodetmp.prev = successor
+			else:
+				self._swap_nodes(successor, successor.right)
+				successor.right = node.right
+				successor.right.prev = successor
+			self._swap_nodes(node, successor)
+			successor.left = node.left
+			successor.left.prev = successor
+			successor.red = node.red
+		if color is False:
+			self._fix_deletion(nodetmp)
+
+	def _fix_deletion(self, node: Node):
+		"""fix tree after node deletion"""
+		while node != self._root and node.red is False:
+			if node == node.prev.left: # check node position
+				nodes = node.prev.right # brother/sister
+				if nodes.red: # brother/sister is red
+					nodes.red = False
+					node.prev.red = True
+					self._rotate_left(node.prev)
+					nodes = node.prev.right
+				if nodes.left.red is False and nodes.right.red is False:
+					nodes.red = True
+					node = node.prev
+				else: # brother/sister is black
+					if nodes.right.red is False: # brother's/sister's right child is black
+						nodes.left.red = False
+						nodes.red = True
+						self._rotate_right(nodes)
+						nodes = node.prev.right
+					nodes.red = node.prev.red
+					node.prev.red = False
+					nodes.right.red = False
+					self._rotate_left(node.prev)
+					node = self._root
+			else: # node in right position from parent
+				nodes = node.prev.left # brother/sister
+				if nodes.red: # brother/sister is red
+					nodes.red = False
+					node.prev.red = True
+					self._rotate_right(node.prev)
+					nodes = node.prev.left
+				if nodes.right.red is False and nodes.left.red is False:
+					nodes.red = True
+					node = node.prev
+				else:
+					if nodes.left.red is False:
+						nodes.right.red = False
+						nodes.red = True
+						self._rotate_left(nodes)
+						nodes = node.prev.left
+					nodes.red = node.prev.red
+					node.prev.red = False
+					nodes.left.red = False
+					self._rotate_right(node.prev)
+					node = self._root
+		node.red = False
+
+	def _find_successor(self, node: Node) -> Node:
+		"""find node successor"""
+		while node.left is not self._nil:
+			node = node.left
+		return node
+
+	def _swap_nodes(self, old: Node, new: Node):
+		"""replace nodes"""
+		if not old.prev:
+			self._root = new
+		elif old == old.prev.left:
+			old.prev.left = new
+		else:
+			old.prev.right = new
+		new.prev = old.prev
 
 	def out(self, root: Node) -> NoReturn:
 		"""display the tree in the terminal. this is not the best solution in terms of
@@ -307,22 +329,20 @@ def main():
 	tree.delete(14)
 	tree.out(tree.get_root())
 	print("\nlenght =", tree.get_lenght())
-	tree.delete(7)
+	tree.delete(10)
 	tree.out(tree.get_root())
 	print("\nlenght =", tree.get_lenght())
-	tree.delete(6)
-	tree.out(tree.get_root())
-	print("\nlenght =", tree.get_lenght())
-	tree.delete(5)
-	tree.out(tree.get_root())
-	print("\nlenght =", tree.get_lenght())
+	tree.out_simple(tree.get_root())
 	tree.delete(11)
 	tree.out(tree.get_root())
 	print("\nlenght =", tree.get_lenght())
-	tree.delete(8)
+	tree.delete(7)
 	tree.out(tree.get_root())
 	print("\nlenght =", tree.get_lenght())
-	tree.delete(13)
+	tree.delete(3)
+	tree.out(tree.get_root())
+	print("\nlenght =", tree.get_lenght())
+	tree.delete(2)
 	tree.out(tree.get_root())
 	print("\nlenght =", tree.get_lenght())
 
